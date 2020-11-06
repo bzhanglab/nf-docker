42585625fc79ad7686bd2b0d7afde0ccbecd80b0
Zhiao Shi <zhiao.shi@gmail.com>
2020-11-06 08:55:22 -0600

    add dockers for neoflow

---
+++ pga/install.R
@@ -0,0 +1,8 @@
+# Install the development version from GitHub:
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+install.packages("remotes")
+BiocManager::install("wenbostar/PGA")
+install.packages("tidyverse")
+install.packages("optparse")
+install.packages("fuzzyjoin")
